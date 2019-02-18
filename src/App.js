fb4881bc4ce0852f942b32d334e7b4d117188f93
InnaZakarzheieva <44522230+InnaZakarzheeva@users.noreply.github.com>
2019-02-18 23:50:40 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default class App extends React.Component {
     ];
     return (
       <div className="App">
-      <ScrollUpButton/>
+     <ScrollUpButton/>
       <ScrollableAnchor id="main">
         <Head_line/>
       </ScrollableAnchor>
